86606daf45c5725a91a41e26120fd18cdda9f658
empipio <94937507+empipio@users.noreply.github.com>
2022-04-06 21:37:53 +0100

    model relationships done

---
+++ models/UserTeam.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class UserTeam extends Model {}
+
+UserTeam.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "User",
+        key: "id",
+        unique: false,
+      },
+    },
+    team_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Teams",
+        key: "id",
+        unique: false,
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user_team",
+  }
+);
+
+module.exports = UserTeam;
