a01a5ba7271878460be2956d39ca1a86b9ac1ca1
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-04-07 14:42:46 +0100

    built home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,23 +2,63 @@ const router = require("express").Router();
 const { Project, Team, User } = require("../models");
 const withAuth = require("../utils/auth.js");
 
-// home route ( / )
+// home routes ( / )
 
-// @TODO - TRY THIS BELOW
-
-// Get all projects for the homepage
+// Get All Projects
 router.get("/", async (req, res) => {
   try {
-    // get all projects and include the user for each project
-    const projectData = await Project.findAll({
-      include: [User],
-    });
+    // ==== @TODO - An error occurs when I { include: [User] } =====
+
+    // get all projects
+    const projectData = await Project.findAll();
     // serialize the data
     const projects = projectData.map((project) => project.get({ plain: true }));
-    // render projects to homepage
-    res.render("homepage", { projects, loggedIn: req.session.loggedIn });
+    // render data to front-end
+    res.json(projects);
+    // catch errors
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+// Get Single Project
+router.get("/project/:id", withAuth, async (req, res) => {
+  try {
+    // get a single project
+    const projectData = await Project.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    // serialize the data
+    const project = projectData.get({ plain: true });
+    // render data to front end
+    res.json(project);
+    // catch errors
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login route
+router.get("/login", (req, res) => {
+  // if user is logged in, redirect to dashboard
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  } else {
+    res.send("please login");
+  }
+});
+
+// Signup Route
+router.get("/signup", (req, res) => {
+  // if user is logged in
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  } else {
+    res.send("please signup");
   }
 });
 
