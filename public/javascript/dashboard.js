69fd379292e8ff85c6e2d716053bdc40e6530af2
empipio <94937507+empipio@users.noreply.github.com>
2022-04-13 21:30:06 +0100

    restriction works on chat but not project-item

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,12 +1,36 @@
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-});
+// $(document).ready(function () {
+//   $(".sidenav").sidenav();
+// });
 
-$(document).ready(function () {
-  $("select").formSelect();
-});
+// $(document).ready(function () {
+//   $("select").formSelect();
+// });
 
+// THIS WORKS ON THE CHAT PAGE BUT WON'T WORK HERE
+// When this is linked to project-item.handlebars it stops the projects from rendering
 
+const checkTeam = () => {
+  // no of designers needed
+  let designers_needed = document.getElementById("designers_needed").innerText;
+  // no of developers needed
+  let developers_needed =
+    document.getElementById("developers_needed").innerText;
+  // no of artist needed
+  let artist_needed = document.getElementById("artist_needed").innerText;
+
+  console.log("restriction function connected");
+  if (
+    designers_needed === 0 &&
+    developers_needed === 0 &&
+    artist_needed === 0
+  ) {
+    window.alert("This team is full!");
+    return;
+  }
+};
+
+const joinButton = document.getElementById("join-team-btn");
+joinButton.addEventListener("click", checkTeam);
 // //
 // const profileBtn = document.getElementById("profile-btn");
 
--- public/javascript/joinproject.js
+++ public/javascript/joinproject.js
@@ -32,11 +32,7 @@ const joinTheProject = async (event) => {
   const myRole = document.getElementById("my-role").innerText;
 
   //need this first if statement on dashboard page
-  if (
-    designers_needed === 0 &&
-    developers_needed === 0 &&
-    artist_needed === 0
-  ) {
+  if (designers_needed <= 0 && developers_needed <= 0 && artist_needed <= 0) {
     window.alert("This team is full!");
     return;
   } else {
